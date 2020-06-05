578752f2e3f67c12726a6a2142ee1596a0fe7e3e
ComplYue <compl.yue@icloud.com>
2020-06-05 18:58:13 +0800

    lift forage entry to top level

--- sedh/forage/Main.hs
+++ sedh/forage/Main.hs
@@ -34,7 +34,7 @@ main = do
         -- install batteries provided by sedh
         installSwarmBatteries starter world
 
-        runEdhModule world "swarm/forage" edhModuleAsIs >>= \case
+        runEdhModule world "forage" edhModuleAsIs >>= \case
           Left !err -> atomically $ do
             -- program crash on error
             consoleOut "Edh swarm forager crashed with an error:\n"

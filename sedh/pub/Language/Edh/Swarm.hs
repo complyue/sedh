d8e5fcd368cf17bb07e3e5be376e01095996454f
Compl Yue <compl.yue@icloud.com>
2020-07-20 22:35:49 +0800

    api chg createEdhDict

--- sedh/pub/Language/Edh/Swarm.hs
+++ sedh/pub/Language/Edh/Swarm.hs
@@ -84,7 +84,8 @@ installSwarmBatteries (SwarmWorkStarter executable workDir workModu managerPid w
               ]
             ]
 
-      artsDict <- createEdhDict [ (EdhString k, v) | (k, v) <- moduArts ]
+      artsDict <- EdhDict
+        <$> createEdhDict [ (EdhString k, v) | (k, v) <- moduArts ]
       updateEntityAttrs pgs (objEntity modu)
         $  [ (AttrByName k, v) | (k, v) <- moduArts ]
         ++ [(AttrByName "__exports__", artsDict)]

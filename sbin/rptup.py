75ba9873a08b54328645f17eef570464d1423aa7
ComplYue <compl.yue@icloud.com>
2021-06-24 23:46:08 +0800

    wip

---
+++ sbin/rptup.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+import subprocess, re, socket, json, sys
+
+
+addr, port = "255.255.255.255", 6768
+try:
+    if len(sys.argv) == 3:
+        port = int(sys.argv[1])
+        addr = sys.argv[2]
+    elif len(sys.argv) == 2:
+        port = int(sys.argv[1])
+except Exception as ex:
+    print("Args error: " + ex, file=sys.stderr)
+    print("Usage: rptup.py [ port [addr] ]", file=sys.stderr)
+    sys.exit(1)
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP) as sock:
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
+
+    mac = None
+    for line in (
+        subprocess.check_output(["/usr/bin/env", "ip", "addr", "show"])
+        .decode("utf-8")
+        .splitlines()
+    ):
+        mac_match = re.search("\s+link/ether (\S+)", line)
+        if mac_match:
+            mac = mac_match.group(1)
+            continue
+
+        if mac is None:
+            continue
+
+        ip_match = re.search("\s+inet (\S+)/", line)
+        if ip_match:
+            ip = ip_match.group(1)
+
+            announcement = json.dumps(
+                {
+                    "mac": mac,
+                    "ip": ip,
+                }
+            )
+
+            print(f"Reporting to [{addr}:{port}]", file=sys.stderr)
+            # print(announcement, file=sys.stderr)
+            sock.sendto(announcement.encode("utf-8"), (addr, port))

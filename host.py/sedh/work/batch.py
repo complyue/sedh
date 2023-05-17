06f5bc183ded8ee6614ddcdb8a678f24d40ecd7d
xl <xlstone46630@gmail.com>
2023-05-17 17:41:42 +0800

    fixup

--- host.py/sedh/work/batch.py
+++ host.py/sedh/work/batch.py
@@ -37,10 +37,14 @@ async def manage_batch_jobs(
         finally:
             outlet.close()
     else:
-        # 接力模式，headhunter由外部传入，即多任务之间不释放召集连接，反复复用
-        hh._settle_result = settle_result
-        async for ips in params():
-            logger.debug(f"Dispatching job ips={ips!r}")
-            await hh.dispatch_job(ips)
-        await hh.finish_jobs()
-        logger.info("All jobs sent out.")
+        try:
+            # 接力模式，headhunter由外部传入，即多任务之间不释放召集连接，反复复用
+            hh._settle_result = settle_result
+            hh.result_ch = outlet
+            async for ips in params():
+                logger.debug(f"Dispatching job ips={ips!r}")
+                await hh.dispatch_job(ips)
+            await hh.finish_jobs()
+            logger.info("All jobs sent out.")
+        finally:
+            outlet.close()
